88c78ec3c071b2983a6db89396fccb69615c5239
Jainam2848 <69759971+Jainam2848@users.noreply.github.com>
2021-05-27 10:37:42 +0530

    Create a2q2.py

---
+++ Assignment 2/a2q2.py
@@ -0,0 +1,14 @@
+# NAME :- SHAH JAINAM DINESHKUMAR
+# NSID :- AYQ754
+# STUDENT NUMBER :- 11321534
+# INSTRUCTOR'S NAME :- Nisha Puthiyedth
+# COURSE :- CMPT 141
+
+string = input("Enter a string :")
+
+even_elements = string[1::2]
+odd_elements = string[0::2]
+
+concatenate = odd_elements + "," + even_elements
+
+print("The result of spliting input string is ",concatenate)
