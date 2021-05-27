1542a46211c221faff33126ae4b0fbcfe3e58b7b
Jainam2848 <69759971+Jainam2848@users.noreply.github.com>
2021-05-27 10:36:48 +0530

    Create a1q1.py

---
+++ Assignment 1/a1q1.py
@@ -0,0 +1,14 @@
+# NAME :- SHAH JAINAM DINESHKUMAR
+# NSID :- AYQ754
+# STUDENT NUMBER :- 11321534
+# INSTRUCTOR'S NAME :- Nisha Puthiyedth
+# COURSE :- CMPT 141
+
+Name = input("Enter your name :")
+Verb = input("Enter a verb :")
+Friend = input("Enter your friend name :")
+
+print(Name, "was", Verb + "ing", "near a lake.")
+print("Suddenly he heard some voice and found", Friend, ".")
+print("Then they shared their ideas and because of having similar ideas",
+      Name, "and", Friend, "became best friends.")
