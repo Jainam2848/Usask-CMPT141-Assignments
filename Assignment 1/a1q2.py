91e188da96ca651e97757cef42b777487eb8c99a
Jainam2848 <69759971+Jainam2848@users.noreply.github.com>
2021-05-27 10:36:55 +0530

    Create a1q2.py

---
+++ Assignment 1/a1q2.py
@@ -0,0 +1,12 @@
+# NAME :- SHAH JAINAM DINESHKUMAR
+# NSID :- AYQ754
+# STUDENT NUMBER :- 11321534
+# INSTRUCTOR'S NAME :- Nisha Puthiyedth
+# COURSE :- CMPT 141
+
+import math as m
+H = float(input("Enter the height from which you want to throw :"))
+time_of_impact = m.sqrt(2 * H / 9.8)
+
+print("The computer dropped from", H,
+      "meters hits the ground after", time_of_impact, "seconds.")
