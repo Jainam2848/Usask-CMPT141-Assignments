983627e7b967842211029ab624ee33c3d281be80
Jainam2848 <69759971+Jainam2848@users.noreply.github.com>
2021-06-21 11:48:49 +0530

    Update a4q2.py

--- Assignment 4/a4q2.py
+++ Assignment 4/a4q2.py
@@ -6,7 +6,7 @@
 
 import numpy as np
 
-#opening file
+# opening file
 a = open("a4q2.txt", 'r')
 
 # creating a empty list and declaring variables
@@ -14,39 +14,37 @@ lst = []
 x = 0
 o = 0
 
-#iterating each line of file and removing newline char
-
+# iterating each line of file and removing newline char
+B = np.loadtxt("a4q2.txt", 'r')
 for line in a.readlines():
     l = [x for x in line.split()]
     lst.append(l)
-    a.close()    
-
-#converting lst to array
+    a.close()
+print(B)
+# converting lst to array
 board = np.array(lst)
 print(board)
 print()
 
-#counting number of x and 0 using nested loops
+# counting number of x and 0 using nested loops
 for i in board:
     for j in i:
-        if j=="x":
-            x+=1
-        if j=="o":
-            o+=1
-   
-#checking if game is legal or not
+        if j == "x":
+            x += 1
+        if j == "o":
+            o += 1
+
+# checking if game is legal or not
 print("x = ", x)
 print("o = ", o)
-if x==o or x-o==1:
+if x == o or x - o == 1:
     print("Game is legal")
 else:
-    #x can be greater than o by 1.. bcoz the x will play the move first
-    if o+1>x:
+    # x can be greater than o by 1.. bcoz the x will play the move first
+    if o + 1 > x:
         print("There are too many x")
         print("Game is illegal")
 
-    elif o>x:
+    elif o > x:
         print("There are too many o")
         print("Game is illegal")
-    
-a.close()    
